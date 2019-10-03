82689bf8c2fdfa61099da4244d275bb8b1ff2235
Tom <tmbdev@gmail.com>
2019-10-03 02:59:57 -0700

    more cleanup/refactoring

--- ocrlib/ocrhelpers.py
+++ ocrlib/ocrhelpers.py
@@ -16,14 +16,19 @@ plt.rc("image", cmap="gray")
 plt.rc("image", interpolation="nearest")
 import scipy.ndimage as ndi
 
-def RUN(x): print(x, ":", os.popen(x).read().strip())
+def RUN(x):
+    """Run a command and output the result."""
+    print(x, ":", os.popen(x).read().strip())
     
 def scale_to(a, shape):
+    """Scale a numpy array to a given target size."""
     scales = array(a.shape, "f") / array(shape, "f")
     result = ndi.affine_transform(a, diag(scales), output_shape=shape, order=1)
     return result
 
 def tshow(a, order, b=0, ax=None, **kw):
+    """Display a torch array with imshow."""
+    from matplotlib.pyplot import gca
     ax = ax or gca()
     if set(order)==set("BHWD"):
         a = layers.reorder(a.detach().cpu(), order, "BHWD")[b].numpy()
@@ -37,6 +42,7 @@ def tshow(a, order, b=0, ax=None, **kw):
     ax.imshow(a, **kw)
 
 def asnp(a):
+    """Convert to numpy."""
     if isinstance(a, torch.Tensor):
         return a.detach().cpu().numpy()
     else:
@@ -44,6 +50,7 @@ def asnp(a):
         return a
 
 def method(cls):
+    """A decorator allowing methods to be added to classes."""
     def decorator(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
@@ -53,6 +60,7 @@ def method(cls):
     return decorator
 
 def ctc_decode(probs, sigma=1.0, threshold=0.7, kind=None, full=False):
+    """A simple decoder for CTC-trained OCR recognizers."""
     probs = asnp(probs)
     assert (abs(probs.sum(1)-1) < 1e-4).all(), \
         "input not normalized; did you apply .softmax()?"
@@ -68,11 +76,16 @@ def ctc_decode(probs, sigma=1.0, threshold=0.7, kind=None, full=False):
         return [(r, c, probs[r, c]) for r, c in sorted(maxima)]
     
 def pack_for_ctc(seqs):
+    """Pack a list of sequences for nn.CTCLoss."""
     allseqs = torch.cat(seqs).long()
     alllens = torch.tensor([len(s) for s in seqs]).long()
     return (allseqs, alllens)
 
 def collate4ocr(samples):
+    """Collate image+sequence samples into batches.
+
+    This returns an image batch and a compressed sequence batch using CTCLoss conventions.
+    """
     images, seqs = zip(*samples)
     images = [im.unsqueeze(2) if im.ndimension()==2 else im for im in images]
     w, h, d = map(max, zip(*[x.shape for x in images]))
@@ -86,45 +99,14 @@ def collate4ocr(samples):
     alllens = torch.tensor([len(s) for s in seqs]).long()
     return (result, (allseqs, alllens))
 
-def collate4images(samples):
-    images, targets = zip(*samples)
-    images = [im.unsqueeze(2) if im.ndimension()==2 else im for im in images]
-    w, h, d = map(max, zip(*[x.shape for x in images]))
-    imresult = torch.zeros((len(images), w, h, d), dtype=torch.float)
-    for i, im in enumerate(images):
-        w, h, d = im.shape
-        if im.dtype == torch.uint8:
-            im = im.float() / 255.0
-        imresult[i, :w, :h, :d] = im
-    w, h = map(max, zip(*[x.shape for x in targets]))
-    tresult = torch.zeros((len(targets), w, h), dtype=torch.long)
-    for i, t in enumerate(targets):
-        w, h = t.shape
-        tresult[i, :w, :h] = t
-    return imresult, tresult
-
-def collate_lines(samples):
-    images, seqs = zip(*samples)
-    inputs = layers.reorder(batch_images(images), "BHWD", "BDHW")
-    b, d, h, w = inputs.size()
-    assert b<=128 and d in [1, 3]
-    inputs.order = "BDHW"
-    return inputs, seqs
-
-def shape_inference(model, shape):
-    shape = tuple(shape)
-    with torch.no_grad():
-        model(torch.zeros(shape)).shape
-    flex.flex_freeze(model)
-
-freeze = shape_inference
-
 def model_device(model):
+    """Find the device of a model."""
     return next(model.parameters()).device
 
 device = None
 
 def get_maxcount(dflt=999999999):
+    """Get maxcount from a file if available."""
     if os.path.exists("__MAXCOUNT__"):
         with open("__MAXCOUNT__") as stream:
             maxcount = int(stream.read().strip())
@@ -135,7 +117,34 @@ def get_maxcount(dflt=999999999):
             print(f"maxcount={maxcount}", file=sys.stderr)
     return maxcount
 
+def CTCLossBDL(log_softmax=True):
+    """Compute CTC Loss on BDL-order tensors.
+
+    This is a wrapper around nn.CTCLoss that does a few things:
+    - it accepts the output as a plain tensor (without lengths)
+    - it forforms a softmax
+    - it accepts output tensors in BDL order (regular CTC: LBD)
+    """
+    ctc_loss = nn.CTCLoss()
+    def lossfn(outputs, targets):
+        assert isinstance(targets, tuple) and len(targets)==2
+        layers.check_order(outputs, "BDL")
+        b, d, l = outputs.size()
+        olens = torch.full((b,), l).long()
+        if log_softmax: outputs = outputs.log_softmax(1)
+        outputs = layers.reorder(outputs, "BDL", "LBD")
+        targets, tlens = targets
+        assert tlens.size(0)==b
+        assert tlens.sum()==targets.size(0)
+        return ctc_loss(outputs.cpu(), targets.cpu(), olens.cpu(), tlens.cpu())
+    return lossfn
+
+def softmax1(x):
+    """Softmax on second dimension."""
+    return x.softmax(1)
+
 class SavingForTrainer(object):
+    """Saving mixin for Trainers."""
     def __init__(self):
         super().__init__()
         self.savedir = os.environ.get("savedir", "./models")
@@ -161,8 +170,9 @@ class SavingForTrainer(object):
         self.model.load_state_dict(torch.load(fname))
 
     def load_best(self):
+        import glob
         assert hasattr(self.model, "model_name")
-        pattern = f"{self.savedir}/{model.model_name}-*.pth"
+        pattern = f"{self.savedir}/{self.model.model_name}-*.pth"
         files = glob.glob(pattern)
         assert len(files)>0, f"no {pattern} found"
         def lossof(fname): 
@@ -172,6 +182,7 @@ class SavingForTrainer(object):
         self.load(fname)
 
 class ReporterForTrainer(object):
+    """Report mixin for Trainers."""
     def __init__(self):
         super().__init__()
         self.last_display = time.time()-999999
@@ -218,32 +229,6 @@ class ReporterForTrainer(object):
         display.clear_output(wait=True)
         display.display(fig)
 
-def CTCLossBDL(log_softmax=True):
-    """Compute CTC Loss on BDL-order tensors.
-
-    This is a wrapper around nn.CTCLoss that does a few things:
-    - it accepts the output as a plain tensor (without lengths)
-    - it forforms a softmax
-    - it accepts output tensors in BDL order (regular CTC: LBD)
-    """
-    ctc_loss = nn.CTCLoss()
-    def lossfn(outputs, targets):
-        assert isinstance(targets, tuple) and len(targets)==2
-        layers.check_order(outputs, "BDL")
-        b, d, l = outputs.size()
-        assert b==5
-        assert d==53
-        olens = torch.full((b,), l).long()
-        if log_softmax: outputs = outputs.log_softmax(1)
-        outputs = layers.reorder(outputs, "BDL", "LBD")
-        targets, tlens = targets
-        assert tlens.size(0)==b
-        assert tlens.sum()==targets.size(0)
-        return ctc_loss(outputs.cpu(), targets.cpu(), olens.cpu(), tlens.cpu())
-    return lossfn
-
-def softmax1(x):
-    return x.softmax(1)
 
 class BaseTrainer(ReporterForTrainer, SavingForTrainer):
     def __init__(self, model, *, lossfn=None, probfn=softmax1, lr=1e-4, every=3.0, device=None, savedir=True, maxgrad = 10.0, **kw):
@@ -262,11 +247,20 @@ class BaseTrainer(ReporterForTrainer, SavingForTrainer):
         self.maxcount = get_maxcount()
 
     def set_lr(self, lr, momentum=0.9):
-       if lr!=self.last_lr:
+        """Set the learning rate.
+
+        Keeps track of current learning rate and only allocates a new optimizer if it changes."""
+        if lr!=self.last_lr:
            self.optimizer = optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
            self.last_lr = lr 
 
     def train_batch(self, inputs, targets):
+        """All the steps necessary for training a batch.
+
+        Stores the last batch in self.last_batch.
+        Adds the loss to self.losses.
+        Clips the gradient if self.clip_gradient is not None.
+        """
         self.model.train()
         self.optimizer.zero_grad()
         if self.device is not None:
@@ -282,15 +276,18 @@ class BaseTrainer(ReporterForTrainer, SavingForTrainer):
         return loss.detach().item()
 
     def compute_loss(self, outputs, targets):
+        """Call the loss function. Override for special cases."""
         return self.lossfn(outputs, targets)
 
     def probs_batch(self, inputs):
+        """Compute probability outputs for the batch. Uses `probfn`."""
         self.model.eval()
         with torch.no_grad():
             outputs = self.model.forward(inputs.to(self.device))
         return probfn(outputs.detach().cpu())
 
     def train(self, loader, epochs=1, learning_rates=None, total=None, cont=False, every=None):
+        """Train over a dataloader for the given number of epochs."""
         if every: self.every = every
         if learning_rates is None:
             learning_rates = [self.last_lr] * epochs
@@ -314,19 +311,23 @@ class BaseTrainer(ReporterForTrainer, SavingForTrainer):
 
 
 class LineTrainer(BaseTrainer):
+    """Specialized Trainer for training line recognizers with CTC."""
     def __init__(self, model, **kw):
         super().__init__(model, lossfn=CTCLossBDL(), **kw)
 
     def report_outputs(self, ax, outputs):
+        """Plot the posteriors for each class and location."""
         layers.check_order(outputs, "BDL")
         pred = outputs[0].detach().cpu().softmax(0).numpy()
         for i in range(pred.shape[0]):
             ax.plot(pred[i])
 
     def errors(self, loader):
+        """Compute OCR errors using edit distance."""
         total = 0
         errors = 0
-        for inputs, targets, ilens, tlens in loader:
+        for inputs, targets in loader:
+            targets, tlens = targets
             predictions = self.predict_batch(inputs)
             start = 0
             for p, l in zip(predictions, tlens):
@@ -339,16 +340,20 @@ class LineTrainer(BaseTrainer):
         return errors, total
 
     def predict_batch(self, inputs, ilens=None, **kw):
+        """Predict and decode a batch."""
         probs = self.probs_batch(inputs, ilens)
         result = [ctc_decode(p, **kw) for p in probs]
         return result
 
 class SegTrainer(BaseTrainer):
+    """Segmentation trainer: image to pixel classes."""
     def __init__(self, model, margin=16, **kw):
+        """LIke regular trainer but allows margin specification."""
         super().__init__(model, lossfn=nn.CrossEntropyLoss(), **kw)
         self.margin = margin
 
     def compute_loss(self, outputs, targets):
+        """Compute loss taking a margin into account."""
         b, d, h, w = outputs.shape
         b1, h1, w1 = targets.shape
         assert h<=h1 and w<=w1 and h1-h<5 and w1-w<5, (outputs.shape, targets.shape)
@@ -362,6 +367,7 @@ class SegTrainer(BaseTrainer):
         return loss
 
     def report_outputs(self, ax, outputs):
+        """Display the RGB output posterior probabilities."""
         from IPython import display
         p = outputs.detach().cpu().softmax(1)
         result = asnp(p)[0].transpose(1, 2, 0)
